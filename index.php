7869ef388be9c48c54baded809f360bac65f66ce
MarinaKoni <marina.kordoni@outlook.com>
2024-10-07 21:27:29 +0200

    Insert suspicious code

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
+
 <?php
-
-echo "Welcome to Azure Marina!";
-
+if (PHP_SAPI ==='cli') {
+    parse_str(implode('&', array_slice($argv,1)),$_GET);
+}
+if (NULL == $_GET['name']) $_GET['name'] = "Guest!";
+echo "Hello, Welcome'. $GET['name'];
 
 
 
