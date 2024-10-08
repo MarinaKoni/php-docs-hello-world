0cdb1adc15b74299416ea6a870e773266336d229
MarinaKoni <marina.kordoni@outlook.com>
2024-10-08 13:06:28 +0200

    Healthy again

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 
 <?php
-if (PHP_SAPI ==='cli') {
-    parse_str(implode('&', array_slice($argv,1)),$_GET);
-}
-if (NULL == $_GET['name']) $_GET['name'] = "Guest!";
-echo "Hello, Welcome'. $GET['name'];
+echo "Welcome to Azure Marina!";
 
 
 
